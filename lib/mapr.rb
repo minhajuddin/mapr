137f02662aabc638dda0d1a280ad875d4e6497c2
Khaja Minhajuddin <minhajuddin.k@gmail.com>
2020-11-18 22:27:17 -0500

    Add more matchers

--- lib/mapr.rb
+++ lib/mapr.rb
@@ -14,7 +14,27 @@ module Mapr
   end
 
   def dig(data, path)
-    data.dig(*path.split("/"))
+    data.dig(*parse_path(path))
+  end
+
+  def parse_path(path)
+    case path
+    when String
+      path.split("/")
+        .map(&method(:transform_path_token))
+    when Integer
+      [path]
+    else
+      raise Mapr::Error, "Path has to be a string or integer path was of type: '#{path.class}' (#{path})"
+    end
+  end
+
+  def transform_path_token(path_token)
+    if path_token =~ /\A\d+\Z/
+      path_token.to_i
+    else
+      path_token
+    end
   end
 
 end
--- spec/mapr_spec.rb
+++ spec/mapr_spec.rb
@@ -28,5 +28,38 @@ RSpec.describe Mapr do
       SCHEMA
       expect(user).to eq(id: 3, first_name: "Danny", last_name: "K")
     end
+
+    it "simple array finders" do
+      user = Mapr.map(<<~SCHEMA, {"User" => {"Ids" => [3, 4], "Name" => {"FirstName" => "Danny", "LastName" => "K"}}})
+      :id: User/Ids/0
+      :first_name: User/Name/FirstName
+      :last_name: User/Name/LastName
+      SCHEMA
+      expect(user).to eq(id: 3, first_name: "Danny", last_name: "K")
+    end
+
+    it "simple array finders with data array" do
+      user = Mapr.map(<<~SCHEMA, [3, 4])
+      :id: 0
+      :second_id: 1
+      SCHEMA
+      expect(user).to eq(id: 3, second_id: 4)
+    end
+
+    it "complex array finders" do
+      user = Mapr.map(<<~SCHEMA, {"Users" => [{"Name" => "Danny"}, {"Name" => "Mujju"}]})
+      :user_1_name: Users/0/Name
+      :user_2_name: Users/1/Name
+      SCHEMA
+      expect(user).to eq(user_1_name: "Danny", user_2_name: "Mujju")
+    end
+
+    it "throws an error for invalid paths" do
+      expect {
+        Mapr.map(<<~SCHEMA, [3, 4])
+        :id: 0.3
+        SCHEMA
+      }.to raise_error(Mapr::Error, /Path has to be a string or integer path was of type: 'Float'/)
+    end
   end
 end
